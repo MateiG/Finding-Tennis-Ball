388d479c230dcbd418c15935be22cb873e3a5d77
MateiG <MateiG@users.noreply.github.com>
2017-10-15 13:21:34 -0400

    Add files via upload

---
+++ separate.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+import time
+
+cap = cv2.VideoCapture('C:/Matei/Python/TENNISBALL/MOREADVANCED/tennisvideo.mp4')
+start = time.time()
+name = 1
+
+while(cap.isOpened()):
+	ret, frame = cap.read()
+
+	cv2.imshow('frame',frame)
+	elapsed = time.time() - start
+
+	if elapsed >= 0.2:
+		
+		start = time.time()
+		cv2.imwrite('C:/Matei/Python/TENNISBALL/MOREADVANCED/video/' + str(name) + '.png', frame)
+		name += 1
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
